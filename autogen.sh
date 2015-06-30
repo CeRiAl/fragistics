fe367543079a8e82bcfd167421266d27e1617764
CeRiAl <ikhatib@gmail.com>
2015-07-01 01:35:28 +0200

    Add autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+REQUIRED_AUTOMAKE_VERSION=1.9
+PKG_NAME=fragistics
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/src/gameslogparser.cpp) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+(cd $srcdir;
+    autoreconf --install --symlink
+)
